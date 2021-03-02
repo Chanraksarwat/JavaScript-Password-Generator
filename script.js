c1b49cc9af75adfed29f42ea9f68811b646b22d9
Chanraksar Wat <chanraksarwat@gmail.com>
2021-03-01 23:13:11 -0600

    Getting password length to generate

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //  Variables that store user inputs
 var input;
+var caseArray = [];
 var lowerCaseChar;
 var upperCaseChar;
 var numberChar;
@@ -8,7 +9,7 @@ var symbolChar;
 // create arrays for user input criteria
 lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "W", "X", "Y", "Z"];
-numericChar=["1","2","3","4","5","6","7","8","9","0"];
+numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 symbolChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "{", "}", "|", "[", "]", ";", "'", ":", "<", ">", "?", "/"];
 
 var generatePassword = document.querySelector("#generate");
@@ -37,7 +38,37 @@ function createPassword() {
     numberChar = confirm("Do you want to include numeric characters?");
     symbolChar = confirm("Do you want to include special characters?");
   }
-}
 
+  // if all validation are trued store in case Array
+  if (useLowerCase == true) {
+    caseArray.push(lowerCaseChar);
+  }
 
-// console.log(createPassword());
+  if (useUpperCase == true) {
+    caseArray.push(upperCaseChar);
+  }
+
+  if (useNumeric == true) {
+    caseArray.push(numericChar);
+  }
+
+  if (useSpecial == true) {
+    caseArray.push(specialChar);
+  }
+  
+  for (let i = 0; i < passwordLength; i++) {
+
+    let randomCharArrayNum;
+    let selectedCharArray;
+    let randomCharNum;
+    let randomChar;
+
+    randomCharArrayNum = parseInt(Math.floor(Math.random() * caseArray.length));
+    selectedCharArray = caseArray[randomCharArrayNum];
+    randomCharNum = Math.floor(Math.random() * selectedCharArray.length);
+    randomChar = selectedCharArray[randomCharNum];
+    passwordString += randomChar;
+  }
+    passwordEntry.textContent = passwordString;
+};
+  
